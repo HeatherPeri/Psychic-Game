ee41d52a367f97b1a5df2fc9c776f7ee2ea3ebed
Heather Peri <heatherhperi@gmail.com>
2019-02-23 13:43:53 -0500

    fixed linking pages

--- assests/Javascript/game.js
+++ assests/Javascript/game.js
@@ -6,7 +6,8 @@
     var guessFar = 0;
     var guessesmade =[];
 
- var letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+ var letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", 
+ "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
     var directionsText = document.getElementById("directions");
     var winsText = document.getElementById("wins");
@@ -19,14 +20,18 @@
         soFar = 0
         guessesMade = [];
     }
-    // var computerGuess = letters 
+    var computerGuess = letters 
  document.onkeyup = function(event) {
     var guess = event.key;
     var ourguess = ourguess[Math.floor(Math.random() * letters.length)];
  }
 
- if (guessesmade === ourguess)
+ if (guessesmade === ourguess){
     wins++;
+ }
+    else{
+        loses++;
+    }
 
 
 
