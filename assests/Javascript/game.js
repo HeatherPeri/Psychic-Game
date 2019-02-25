7b42f140ad1ff513fe7949b31fcb60bde328645c
Heather Peri <heatherhperi@gmail.com>
2019-02-25 17:17:55 -0500

    edited a few syntax issues

--- assests/Javascript/game.js
+++ assests/Javascript/game.js
@@ -2,22 +2,22 @@
 
     var wins = 0;
     var loses = 0;
-    var guessesLeft = 0;
-    var soFar = 0;
+    var guessesleft = 0;
+    var sofar = 0;
     var guessesmade =[];
 
  var letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", 
  "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
-    var directionsText = document.getElementById("directions");
-    var winsText = document.getElementById("wins");
-    var losesText = document.getElementById("loses");
-    var guessesleftText = document.getElementById("guessesleft");
-    var guessesmadeText = document.getElementById("userguesses");
+    var directionsText = document.getElementById("directions-text");
+    var winsText = document.getElementById("wins-text");
+    var guessesleftText = document.getElementById("guessesleft-text");
+    var guessesmadeText = document.getElementById("userguesses-text");
+    var losesText = document.getElementById("loses-text");
 
     function reset() {
         guessesleft = 15;
-        soFar = 0;
+        sofar = 0;
         guessesmade = [];
     }
     // var computerGuess = letters 
@@ -33,17 +33,17 @@
         wins++;
         console.log(wins +"you won")
     }
-    else{
+    if else{
         loses++;
         console.log(loses +"you lost")
-    
+    }
  
 
- winsText.textContent = "You Won: " + wins;
- losesText.textContent = "You Lost" + loses;
- guessesleftText.textContent = "Guesses Left:" + guessesLeft;
- guessesmadeText.textContent = "losses: " + loses;
-    }
+ winsText.textContent = wins;
+ losesText.textContent = loses;
+ guessesleftText.textContent = guessesLeft;
+ guessesmadeText.textContent = guessesmade;
+
  };
 
 
